5679d82a52beac23c83b88972b0c097ec3d2b2a6
Julio <julioa@tecgraf.puc-rio.br>
2018-04-06 17:19:34 -0300

    tetrahedron centroid program updated

--- centroid.cpp
+++ centroid.cpp
@@ -1,52 +1,52 @@
 // example: one class, two objects
 #include <iostream>
+#include <array>
 
 class tetrahedron  {
-    double cx[4];
-    double cy[4];
-    double cz[4];
-  public:
-    void set_values (double,double,double,double);
-    double * centroid (){
-    	double GC[3];
+    std::array<double, 3> n1;
+    std::array<double, 3> n2;
+    std::array<double, 3> n3;
+    std::array<double, 3> n4;
 
-    	GC[0] = (cx [0] + cx [1] + cx [2] + cx [3])/4;
-    	GC[1] = (cy [0] + cy [1] + cy [2] + cy [3])/4;
-    	GC[2] = (cz [0] + cz [1] + cz [2] + cz [3])/4;
+public:
+    //set values of coordinate points of the tetrahedron 
+    void set_values (std::array<double, 3>,std::array<double, 3>,std::array<double, 3>,std::array<double, 3>);
+    //calculate the centroid coordinates of the tetrahedron
+    void  centroid (std::array<double, 3>  & GC){
 
-    	return GC;
+        GC[0] = (n1 [0] + n2 [0] + n3 [0] + n4 [0])/4;
+        GC[1] = (n1 [1] + n2 [1] + n3 [1] + n4 [1])/4;
+        GC[2] = (n1 [2] + n2 [2] + n3 [2] + n4 [2])/4;
+
+    }
+   //print the centroid coordinates of the tetrahedron
+    void print_centroid_coord(const std::array<double, 3> &n)
+    {
+        std::cout << "centroid coordinated of tetrahedron"<< std::endl;
+        for (int j = 0; j < n.size(); j++ )
+        {
+            std::cout << "GC[" << j << "] = " << n[j] << std::endl;
+        }
     }
 };
-
-void tetrahedron ::set_values (double n1, double n2, double n3, double n4) {
-	cx [0]  = n1[0]; 
-	cx [1]  = n2[0];
-	cx [2]  = n3[0]; 
-	cx [3]  = n2[0];
-	cy [0]  = n1[1]; 
-	cy [1]  = n2[1];
-	cy [2]  = n3[1]; 
-	cy [3]  = n2[1];
-	cz [0]  = n1[2]; 
-	cz [1]  = n2[2];
-	cz [2]  = n3[2]; 
-	cz [3]  = n2[2];
+// constructor
+void tetrahedron ::set_values (std::array<double, 3> p1,std::array<double, 3>p2,std::array<double, 3>p3,std::array<double, 3>p4)
+{
+    n1 = p1;
+    n2 = p2;
+    n3 = p3;
+    n4 = p4;
 }
 
 int main () {
-  tetrahedron tet;
-  double n1[3], n2[3], n3[3], n4[3];
-  n1[3] = {3,2,-1};
-  n2[3] = {5,0,2};
-  n3[3] = {-4,5,7};
-  n4[3] = {-2,4,1};
-  tet.set_values (n1,n2,n3,n4);
-  double *gc;
-  gc = tet.centroid();
-  for ( int i = 0; i < 3; i++ ) {
-      cout << "*(gc + " << i << ") : ";
-      cout << *(gc + i) << endl;
-   }
- 
-  return 0;
+    tetrahedron tet;
+    std::array<double, 3> a1{ {3,2,-1} };
+    std::array<double, 3> a2{ {5,0, 2} };
+    std::array<double, 3> a3{ {-4,5,7} };
+    std::array<double, 3> a4{ {-2,4,1} };
+
+    tet.set_values (a1,a2,a3,a4);
+    std::array<double, 3> gc;
+    tet.centroid(gc);
+    tet.print_centroid_coord(gc);
 }
