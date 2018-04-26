8be41f5b5b43e7b22ff4d3292635852e8148fd59
Julio <julioa@tecgraf.puc-rio.br>
2018-04-26 16:05:01 -0300

    tetrahedron class v1.0 and cmake file

---
+++ include/tetrahedron.h
@@ -0,0 +1,30 @@
+#ifndef GMESHLEM_TETRAHEDRON_H_
+#define GMESHLEM_TETRAHEDRON_H_
+
+//!  A Tetrahedron class.
+//! \brief Base class that stores the information about nodes
+//! \details Tetrahedron centroide is calculated
+class Tetrahedron {
+    public:
+        // Constructor with id and coordinates
+        //! \param[in] id assign as the id_ of the tetrahedron
+        //! \param[in] coords coordinates of the tetrahedron nodes
+        Tetrahedron(unsigned id, const std::array<std::array<double, 3>, 4> &coords);
+        //! compute centroid coordinates of a tetrahedron
+        void  compute_centroid();
+        //! Return centroid coordinates
+        //! \retval centroid_ return coordinates of the tetrahedron centroide
+        std::array<double, 3> centroid() const;
+        //! Return id of the tetrahedron
+        unsigned id() const { return id_; }
+
+    private:
+        //! id of the tetrahedron
+        unsigned id_;
+        //! array of the centroide coordinates
+        std::array<double, 3> centroid_;
+        //! array with the nodal coordinates of the tetrahedron
+        std::array<std::array<double, 3>, 4> coord_points_;
+};
+
+#endif //GMESHLEM_TETRAHEDRON_H_
---
+++ src/Nodes.cpp
@@ -0,0 +1,8 @@
+#include "Nodes.h"
+#include <array>
+#include <iostream>
+
+Nodes::Nodes(){};
+Nodes::Nodes(unsigned id, const std::array<double, 3>coords)
+{id_=id; coordinates_=coords;};
+
